a26d4f25ab115191922d4dd4764df49af261e5e7
Nima Dekhli <nima@dekhli.ch>
2024-03-12 11:32:58 +0100

    init project

--- src/wallet.py
+++ src/wallet.py
@@ -1,4 +1,4 @@
-"""Module to create and manipulate a wallet"""
+"""Module that lets you manipulate a wallet"""
 
 
 class InsufficientAmount(Exception):
@@ -10,28 +10,23 @@ class InsufficientAmount(Exception):
 class Wallet(object):
     """Wallet class that lets your create a Wallet and add or spend money"""
 
-    def __init__(self, initial_amount:float=0):
+    def __init__(self, initial_amount=0):
         self.balance = initial_amount
-        self.deferred_balance = 0
 
-    def spend_cash(self, amount: float, deferred=False) -> None:
+    def spend_cash(self, amount, deferred=False):
         """Removes the specified amount of money from the current Wallet object"""
-        if self.balance - self.deferred_balance < amount:
+        if self.balance < amount:
             raise InsufficientAmount(f"Not enough available to spend {amount}")
-        if deferred:
-            self.deferred_balance += amount
-        else:
-            self.balance -= amount
 
-    def add_cash(self, amount: float) -> None:
+        self.balance -= amount
+
+    def add_cash(self, amount):
         """Adds the specified amount of money to the current Wallet object"""
         self.balance += amount
 
-    def transfer(self, wallet, amount:float) -> None:
-        """Transfers a certain amount from the current wallet object to another one"""
-        self.spend_cash(amount)
-        wallet.add_cash(amount)
+    def spend_money(self, amount, deferred=False):
+        """Removes the specified amount of money from the current Wallet object"""
+        if self.balance < amount:
+            raise InsufficientAmount(f"Not enough available to spend {amount}")
 
-    def pay_deferred_payments(self):
-        """Makes deferred payments effective"""
-        self.spend_cash(self.deferred_balance)
+        self.balance -= amount
--- tests/unit/test_wallet.py
+++ tests/unit/test_wallet.py
@@ -8,37 +8,27 @@ from tests.fixtures.wallet_fixtures import empty_wallet, wallet
 
 def test_default_initial_amount(empty_wallet):
     """Tests that initial balance of empty_wallet is 0"""
-    assert empty_wallet.balance == 0.0
+    assert empty_wallet.balance == 0
 
 
 def test_setting_initial_amount(wallet):
     """Tests that initial ballance of wallet is 20"""
-    assert wallet.balance == 20.0
+    assert wallet.balance == 20
 
 
 def test_wallet_add_cash(wallet):
-    """Tests add_cash function"""
-    amount_to_add = 32.55
-    expected_balance = wallet.balance + amount_to_add
-
-    wallet.add_cash(amount_to_add)
-
-    assert wallet.balance == expected_balance
+    """Tests add_cash funciton"""
+    wallet.add_cash(80)
+    assert wallet.balance == 100
 
 
 def test_wallet_spend_cash(wallet):
     """Tests spend_cash function"""
-    amount_to_spend = 15.3
-    expected_balance = wallet.balance - amount_to_spend
-
-    wallet.spend_cash(amount_to_spend)
-
-    assert wallet.balance == expected_balance
+    wallet.spend_cash(10)
+    assert wallet.balance == 10
 
 
 def test_wallet_spend_cash_raises_exception_on_insufficient_amount(empty_wallet):
     """Tests that InsufficientAmount exception is raised when trying to spend more thant the wallet's balance"""
-    amount_to_spend = 15.0
-
     with pytest.raises(InsufficientAmount):
-        empty_wallet.spend_cash(amount_to_spend)
+        empty_wallet.spend_cash(100)
