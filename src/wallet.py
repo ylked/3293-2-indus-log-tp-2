92e3cdde73dc714c4ab643c77cc39f231a81cdc9
Nima Dekhli <nima@dekhli.ch>
2024-03-26 10:34:27 +0100

    remove duplicate method

--- src/wallet.py
+++ src/wallet.py
@@ -24,13 +24,6 @@ class Wallet(object):
         """Adds the specified amount of money to the current Wallet object"""
         self.balance += amount
 
-    def spend_money(self, amount, deferred=False):
-        """Removes the specified amount of money from the current Wallet object"""
-        if self.balance < amount:
-            raise InsufficientAmount(f"Not enough available to spend {amount}")
-
-        self.balance -= amount
-
     def get_balance(self):
         """
         Returns the current balance of the wallet
