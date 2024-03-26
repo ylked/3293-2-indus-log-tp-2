fe3f9beb9842a43751aef0abd625ddcde774cf61
Nima Dekhli <nima@dekhli.ch>
2024-03-26 10:39:00 +0100

    use get_balance() method in unit tests

--- tests/unit/test_wallet.py
+++ tests/unit/test_wallet.py
@@ -8,24 +8,24 @@ from tests.fixtures.wallet_fixtures import empty_wallet, wallet
 
 def test_default_initial_amount(empty_wallet):
     """Tests that initial balance of empty_wallet is 0"""
-    assert empty_wallet.balance == 0
+    assert empty_wallet.get_balance() == 0
 
 
 def test_setting_initial_amount(wallet):
     """Tests that initial ballance of wallet is 20"""
-    assert wallet.balance == 20
+    assert wallet.get_balance() == 20
 
 
 def test_wallet_add_cash(wallet):
     """Tests add_cash funciton"""
     wallet.add_cash(80)
-    assert wallet.balance == 100
+    assert wallet.get_balance() == 100
 
 
 def test_wallet_spend_cash(wallet):
     """Tests spend_cash function"""
     wallet.spend_cash(10)
-    assert wallet.balance == 10
+    assert wallet.get_balance() == 10
 
 
 def test_wallet_spend_cash_raises_exception_on_insufficient_amount(empty_wallet):
